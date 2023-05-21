b36d9490703e60b7ea748935777d05d958e1a0d9
Андрей Ситников <bongaru@gmail.com>
2023-05-21 17:14:25 +0500

    Add pagination to answers moderation page

--- backend/game/views.py
+++ backend/game/views.py
@@ -5,7 +5,8 @@ from .models import Answer
 
 class AnswerListView(ListView):
     model = Answer
+    paginate_by = 20
     template_name = 'answer_list.html'
 
     def get_queryset(self):
-        return Answer.objects.unbound().with_topic_entities()
+        return Answer.objects.unbound().with_topic_entities().order_by('-sent_at')
