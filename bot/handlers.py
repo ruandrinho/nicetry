6bd3894cf9c7cf1d3edfe3581666246543972ecb
Андрей Ситников <bongaru@gmail.com>
2023-06-06 22:52:52 +0500

    Implement hits_mode

--- bot/handlers.py
+++ bot/handlers.py
@@ -41,6 +41,7 @@ redis = Redis(host='redis', decode_responses=True)
 class GameStates(StatesGroup):
     main = State()
     topic = State()
+    mode = State()
     answer = State()
     feedback = State()
     interruption = State()
@@ -281,33 +282,54 @@ async def handle_query_topic(callback: CallbackQuery, state: FSMContext) -> None
         topic_id = int(callback.data.replace('id', ''))
         topic_title = user_data['topics'][str(topic_id)]  # no json deserialization from redis
         await state.update_data(topic_id=topic_id, topic_title=topic_title)
-    else:
-        topic_id = user_data['topic_id']
-        topic_title = user_data['topic_title']
+    new_message = await callback.message.answer(
+        Messages.choose_mode,
+        reply_markup=await get_keyboard([['points_mode', 'hits_mode']])
+    )
+    await state.update_data(new_message_id=new_message.message_id)
+    await callback.message.delete()
+    await callback.answer()
+    await state.set_state(GameStates.mode)
+
+
+@game_router.callback_query(Text(endswith='mode'), GameStates.mode)
+async def handle_query_mode(callback: CallbackQuery, state: FSMContext) -> None:
+    hits_mode = callback.data == 'hits_mode'
+    user_data = await state.get_data()
     async with aiohttp.ClientSession() as session:
         async with session.post(
             f'{API}/round',
             json={
                 'player_id': user_data['player']['id'],
-                'topic_id': topic_id
+                'topic_id': user_data['topic_id'],
+                'hits_mode': hits_mode
             }
         ) as response:
             decoded_response = await response.json()
+            mode_caption = 'hits_mode' if hits_mode else 'points_mode'
             if response.status == 200:
                 logger.info(f'GAME Started round {decoded_response["id"]} by {format_player(user_data["player"])} '
-                            f'on {topic_title}')
+                            f'on {user_data["topic_title"]} ({mode_caption})')
             else:
                 await state.update_data(challenged_topic_id=None, referrer_id=None)
                 await handle_error_response(callback, state, decoded_response['detail'])
-                logger.warning(f'GAME Starting round by {format_player(user_data["player"])} on {topic_title}: '
-                               f'{decoded_response["detail"]}')
+                logger.warning(f'GAME Starting round by {format_player(user_data["player"])} '
+                               f'on {user_data["topic_title"]} ({mode_caption}): {decoded_response["detail"]}')
                 return
             round = await response.json()
-            await state.update_data(round_id=round['id'], hits=[], score1=0, score2=0)
-    pinned_message = await callback.message.answer(f'Тема: {topic_title}')
+            await state.update_data(
+                round_id=round['id'],
+                hits=[],
+                hits_mode=hits_mode,
+                score1=0,
+                score2=0,
+                hits1=0,
+                hits2=0
+            )
+    pinned_message = await callback.message.answer(f'Тема: {user_data["topic_title"]}')
     await pinned_message.pin(disable_notification=True)
     await state.update_data(pinned_message_id=pinned_message.message_id)
-    new_message = await callback.message.answer(Messages.left5)
+    new_message = await callback.message.answer(Messages.hits_mode_first if hits_mode else Messages.attempt1)
     await state.update_data(new_message_id=new_message.message_id)
     await callback.message.delete()
     await callback.answer()
@@ -396,11 +418,24 @@ async def handle_bot_answer(message: Message, state: FSMContext, bot: Bot, resul
     await message.answer(bot_answer)
     await asyncio.sleep(1)
     await handle_hit(message, state, result['bot_answer_entity'], from_bot=True, bot=bot)
-    attempts_left_message = getattr(Messages, f'left{result["attempts_left"]}')
-    new_message = await message.answer(attempts_left_message)
+    user_data = await state.get_data()
+    if user_data['hits_mode']:
+        if user_data['hits1'] == 3 or user_data['hits2'] == 3:
+            attempt_message = Messages.attempt6
+        elif result['attempt'] == 2:
+            attempt_message = Messages.hits_mode_second
+        else:
+            attempt_message = choice(Messages.hits_mode_attempt)
+    else:
+        attempt_message = getattr(Messages, f'attempt{result["attempt"]}')
+    new_message = await message.answer(attempt_message)
     await state.update_data(new_message_id=new_message.message_id)
-    if not result['attempts_left']:
-        await handle_results(message, state, bot)
+    if user_data['hits_mode']:
+        if user_data['hits1'] < 3 and user_data['hits2'] < 3:
+            return
+    elif result['attempt'] < 6:
+        return
+    await handle_results(message, state, bot)
 
 
 async def handle_hit(
@@ -411,7 +446,7 @@ async def handle_hit(
         bot: Bot = None
 ) -> None:
     user_data = await state.get_data()
-    score1, score2 = user_data['score1'], user_data['score2']
+    score1, score2, hits1, hits2 = user_data['score1'], user_data['score2'], user_data['hits1'], user_data['hits2']
     hit_record = {
         'position': entity['position'],
         'answer': entity['title'],
@@ -426,21 +461,24 @@ async def handle_hit(
         if hit_record['points']:
             await state.update_data(last_hit_position=hit_record['position'])
             score1 = score1 + hit_record['points']
-            await state.update_data(score1=score1)
+            hits1 += 1
+            await state.update_data(score1=score1, hits1=hits1)
         return
     last_hit_positions = []
     if hit_record['points']:
         last_hit_positions.append(hit_record['position'])
         score2 = score2 + hit_record['points']
-        await state.update_data(score2=score2)
+        hits2 += 1
+        await state.update_data(score2=score2, hits2=hits2)
     if 'last_hit_position' in user_data and user_data['last_hit_position']:
         last_hit_positions.append(user_data['last_hit_position'])
     await state.update_data(last_hit_position=None)
     new_message = await message.answer(await format_hits(hits, last_hit_positions=last_hit_positions))
     await state.update_data(new_message_id=new_message.message_id)
     with suppress(TelegramBadRequest):
+        score_message = f'{hits1}:{hits2}' if user_data['hits_mode'] else f'{score1}:{score2}'
         await bot.edit_message_text(
-            text=html.bold(f'{score1}:{score2} | {user_data["topic_title"]}'),
+            text=html.bold(f'{score_message} | {user_data["topic_title"]}'),
             chat_id=message.chat.id,
             message_id=user_data['pinned_message_id']
         )
@@ -449,14 +487,30 @@ async def handle_hit(
 async def handle_results(message: Message, state: FSMContext, bot: Bot) -> None:
     user_data = await state.get_data()
     referral = await encode_referral(user_data['topic_id'], user_data['player']['id'])
-    score1, score2 = user_data['score1'], user_data['score2']
-    outcome = f'{score1}:{score2}'
-    if score1 == score2:
-        outcome_message = Messages.outcome_draw.replace('RESULT', outcome)
-    elif score1 > score2:
-        outcome_message = Messages.outcome_victory.replace('RESULT', outcome)
+    score1, score2, hits1, hits2 = user_data['score1'], user_data['score2'], user_data['hits1'], user_data['hits2']
+    if user_data['hits_mode']:
+        outcome = f'{hits1}:{hits2}'
+        if hits1 == hits2:
+            outcome += f' (по очкам {score1}:{score2})'
+            if score1 == score2:
+                outcome_message = Messages.outcome_draw
+            elif score1 > score2:
+                outcome_message = Messages.outcome_victory
+            else:
+                outcome_message = Messages.outcome_defeat
+        elif hits1 > hits2:
+            outcome_message = Messages.outcome_victory
+        else:
+            outcome_message = Messages.outcome_defeat
     else:
-        outcome_message = Messages.outcome_defeat.replace('RESULT', outcome)
+        outcome = f'{score1}:{score2}'
+        if score1 == score2:
+            outcome_message = Messages.outcome_draw
+        elif score1 > score2:
+            outcome_message = Messages.outcome_victory
+        else:
+            outcome_message = Messages.outcome_defeat
+    outcome_message = outcome_message.replace('RESULT', outcome)
     challenge_message = Messages.challenge.replace('TOPIC', user_data['topic_title']).replace('RESULT', outcome_message)
     deeplink = f'https://t.me/NiceTryGameBot?start={referral}'
     formatted_results = await format_hits(user_data['hits'])
@@ -478,7 +532,9 @@ async def handle_results(message: Message, state: FSMContext, bot: Bot) -> None:
             json={
                 'round_id': user_data['round_id'],
                 'score1': score1,
-                'score2': score2
+                'score2': score2,
+                'hits1': hits1,
+                'hits2': hits2
             }
         ):
             logger.info(f'GAME Finished round {user_data["round_id"]} with {outcome}')
